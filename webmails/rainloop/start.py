6ebe5cd03a098cd8552eb224ca425aa585b7c910
fjcloud <fj@msl.cloud>
2020-01-05 13:22:04 +0100

    Update start.py

--- webmails/rainloop/start.py
+++ webmails/rainloop/start.py
@@ -8,8 +8,8 @@ from socrate import system, conf
 
 log.basicConfig(stream=sys.stderr, level=os.environ.get("LOG_LEVEL", "WARNING"))
 
-conf.jinja("/default.ini", os.environ, "/data/_data_/_default_/domains/default.ini")
-conf.jinja("/application.ini", os.environ, "/data/_data_/_default_/configs/application.ini")
+#conf.jinja("/default.ini", os.environ, "/data/_data_/_default_/domains/default.ini")
+#conf.jinja("/application.ini", os.environ, "/data/_data_/_default_/configs/application.ini")
 conf.jinja("/php.ini", os.environ, "/usr/local/etc/php/conf.d/rainloop.ini")
 
 os.execv("/usr/local/bin/apache2-foreground", ["apache2-foreground"])
